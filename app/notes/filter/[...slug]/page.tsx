ac9a989672296bca7d8b0f44001fdb55df881bee
Viktor <victorharasymiv97@gmail.com>
2025-06-30 01:14:05 +0200

    Initial commit

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -14,6 +14,28 @@ type Props = {
   params: Promise<{ slug: string[] }>;
 };
 
+export async function generateMetadata() {
+  return {
+    title: `NoteHub | Notes`,
+    description: "NoteHub list",
+    openGraph: {
+      title: `NoteHub | Notes`,
+      description: "NoteHub list",
+      url: `https://notehub.com/notes/`,
+      siteName: "NoteHub",
+      images: [
+        {
+          url: "https://ac.goit.global/fullstack/react/og-meta.jpg",
+          width: 1200,
+          height: 630,
+          alt: "Note App image",
+        },
+      ],
+      type: "article",
+    },
+  };
+}
+
 const Notes = async ({ params }: Props) => {
   const { slug } = await params;
   const category = slug[0] === "All" ? "" : slug[0];

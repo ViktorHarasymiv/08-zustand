61beac174a4cde0e18f23044743acd3662f06504
Viktor <victorharasymiv97@gmail.com>
2025-07-02 01:46:28 +0200

    Update 1:4

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,7 @@
 import { Metadata } from "next";
 
 import "./globals.css";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import NotFoundClient from "./not-found.client";
 
 export const metadata: Metadata = {
   title: "Not found page",
@@ -10,26 +9,9 @@ export const metadata: Metadata = {
 };
 
 const NotFound = () => {
-  const router = useRouter();
-  const [count, setCount] = useState(3);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCount((prev) => {
-        if (prev === 1) {
-          clearInterval(interval);
-          router.push("/");
-        }
-        return prev - 1;
-      });
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [router]);
-
   return (
     <div className="error">
-      <h1>404 - Сторінку не знайдено</h1>
-      <p>Вас буде перенаправлено на головну через {count} секунд…</p>
+      <NotFoundClient />
     </div>
   );
 };
